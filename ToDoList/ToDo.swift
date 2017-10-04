dbb9c4e23441230dff12e661bb18c59c846acf8d
AA1 <aa1@AA1s-Mac.local>
2017-10-04 02:10:57 -0300

    ToDo Model Created.

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by AA1 on 04/10/17.
+//  Copyright © 2017 AA1. All rights reserved.
+//
+import Foundation
+
+struct ToDo {
+    var title: String
+    var isComplete: Bool
+    var dueDate: Date
+    var notes: String
+}
